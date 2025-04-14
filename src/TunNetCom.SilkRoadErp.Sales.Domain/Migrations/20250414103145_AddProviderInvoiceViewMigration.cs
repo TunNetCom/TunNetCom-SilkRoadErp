810d2fe432c9df58f95a8ee4be9534d1c8fc3d35
Hanine-Benfarhat <hanine.benfarhat@outlook.com>
2025-04-14 12:46:33 +0100

    Add EF Core design package and new migrations #150 #132
    - Updated `Sales.Api.csproj` to include `Microsoft.EntityFrameworkCore.Design` version `9.0.4`.
    - Added `InitMigration` to create tables: `Client`, `Fournisseur`, `Produit`, etc., with foreign key relationships.
    - Introduced `AddProviderInvoiceViewMigration` to create a SQL view for provider invoices.
    - Added `AddReceiptNoteViewMigration` to create a SQL view for receipt notes.
    - Updated `SalesContextModelSnapshot` to reflect the new database model state.

---
+++ src/TunNetCom.SilkRoadErp.Sales.Domain/Migrations/20250414103145_AddProviderInvoiceViewMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TunNetCom.SilkRoadErp.Sales.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProviderInvoiceViewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+                migrationBuilder.Sql(@"
+            CREATE VIEW [dbo].[ProviderInvoiceView]
+AS
+SELECT dbo.FactureFournisseur.Num, dbo.FactureFournisseur.id_fournisseur AS ProviderId, dbo.FactureFournisseur.NumFactureFournisseur AS ProviderInvoiceNumber, dbo.FactureFournisseur.dateFacturationFournisseur AS InvoicingDate, 
+                  dbo.FactureFournisseur.date AS Date, SUM(dbo.LigneBonReception.tot_HT) AS TotalHT, SUM(dbo.LigneBonReception.tot_TTC) AS TotalTTC
+FROM     dbo.FactureFournisseur INNER JOIN
+                  dbo.BonDeReception ON dbo.FactureFournisseur.Num = dbo.BonDeReception.Num_Facture_fournisseur INNER JOIN
+                  dbo.LigneBonReception ON dbo.BonDeReception.Num = dbo.LigneBonReception.Num_BonRec
+GROUP BY dbo.FactureFournisseur.Num, dbo.FactureFournisseur.id_fournisseur, dbo.FactureFournisseur.NumFactureFournisseur, dbo.FactureFournisseur.dateFacturationFournisseur, dbo.FactureFournisseur.date
+
+        ");
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW [dbo].[ProviderInvoiceView]");
+        }
+    }
+}
