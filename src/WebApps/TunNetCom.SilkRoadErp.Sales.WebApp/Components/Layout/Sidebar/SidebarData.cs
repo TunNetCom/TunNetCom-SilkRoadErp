4f5e1700004850d514c2acf24c0191c2b0b2302c
Hanine-Benfarhat <hanine.benfarhat@outlook.com>
2025-05-10 15:41:34 +0100

    Enhance Blazor app layout and sidebar functionality
    Significant updates to the layout and styling of the Blazor application, including the addition of new stylesheets in `App.razor` and restructuring of `MainLayout.razor` to incorporate a sidebar component with toggle and collapsible menu features.
    
    Improved styling in `AddOrUpdateDeliveryNote.razor`, consistent header structures in `ManageInvoices.razor` and `ManageProvidersInvoices.razor`, and updated router syntax in `Routes.razor`.
    
    Introduced new components: `FooterComponent.razor`, `MenuItem.cs`, `MenuItemComponent.razor`, `SidebarComponent.razor`, `SidebarData.cs`, and `SidebarFooterComponent.razor` for better navigation and user experience.
    
    Added `Dashboard.razor` for displaying sales revenue data and updated `style.css` for cohesive design across the application.

---
+++ src/WebApps/TunNetCom.SilkRoadErp.Sales.WebApp/Components/Layout/Sidebar/SidebarData.cs
@@ -0,0 +1,61 @@
+﻿namespace BlazorProTemplate.Components.Layout.Sidebar;
+
+public class SidebarData
+{
+
+    public static List<MenuItem> GetStandardMenuItems() => new()
+    {
+        new MenuItem(href:"/", title: "Home", icon:"ri-home-3-fill"),
+        new MenuItem(href:"/dashboard", title: "Dashboard", icon:"ri-dashboard-fill"),
+        //new MenuItem(href:"/weather", title: "Weather", icon:"ri-bar-chart-horizontal-line"),
+    };
+
+    public static List<MenuItem> GetGeneralMenuItems() => new()
+    {
+        new(title:"Clients", icon:"ri-team-fill",  childMenuItems:
+        [
+            new MenuItem(href:"/customers_list", title:"Consulter Clients"),
+            new MenuItem(title:"Facturation", childMenuItems:
+            [
+                new MenuItem(href:"/manage-invoice", title:"Gestion Facture"),
+
+                new MenuItem(title:"Bon de livraison", childMenuItems:
+                [
+                   new MenuItem(href:"/addOrUpdateDeliveryNote", title:"Gestion Bons de Livraison"),
+                   new MenuItem(href:"/delivery-notes", title:"Consulter Bons de Livraison"),
+                   new MenuItem(href:"/delivery-notes", title:"Devis"),
+                   new MenuItem(title:"More", childMenuItems:
+                   [
+                      
+                   ]),
+                ]),
+                new MenuItem(href:"/delivery-notes", title:"Devis"),
+
+            ]),
+        ]),
+
+        new MenuItem(title:"Fournisseurs", icon:"ri-store-2-fill", childMenuItems:
+        [
+            new MenuItem(href:"/providers_list", title:"Consulter Fournisseurs"),
+            new MenuItem(href:"/manage-providers-invoices", title:"Gestion Facture Fournisseurs"),
+            new MenuItem(href:"#", title:"Commandes Fournisseurs"),
+        ]),
+
+        //new MenuItem(title:"Stock", icon:"ri-shopping-cart-fill", childMenuItems:
+        //[
+        //    new MenuItem(href:"#", title:"Consulter Stock"),
+        //]),
+
+        new MenuItem(title:"Maps", icon:"ri-global-fill", childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Google maps"),
+            new MenuItem(href:"#", title:"Open street map"),
+        ]),
+        new MenuItem(title:"Theme", icon:"ri-paint-brush-fill", childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Dark"),
+            new MenuItem(href:"#", title:"Light"),
+        ]),
+    };
+
+}
